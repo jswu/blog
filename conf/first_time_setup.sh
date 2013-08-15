abe16459c64f0036eb54247fd90460e543f85b53
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2013-08-15 13:43:14 -0400

    Add jekyll to Gemfile

--- conf/first_time_setup.sh
+++ conf/first_time_setup.sh
@@ -3,8 +3,10 @@
 # Terminate script on error
 set -e
 
+CONF_DIR=$(dirname $0)
+
 echo "Setup compass"
-( cd conf && compass init --config config.rb )
+( cd $CONF_DIR && compass init --config config.rb )
 
 echo "Setup bundle"
-( cd conf && bundle install )
+( cd $CONF_DIR && bundle install )
