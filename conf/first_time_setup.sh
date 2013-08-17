6ba02537dc4471350a7ca76d89be47d43e8dcdd4
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2013-08-17 19:37:33 +0000

    Move set up lines around

--- conf/first_time_setup.sh
+++ conf/first_time_setup.sh
@@ -5,8 +5,8 @@ set -e
 
 CONF_DIR=$(dirname $0)
 
-echo "Setup compass"
-( cd $CONF_DIR && compass init --config config.rb )
-
 echo "Setup bundle"
 ( cd $CONF_DIR && bundle install )
+
+echo "Setup compass"
+( cd $CONF_DIR && compass init --config config.rb )
